bacff14f7e134a37739a0a5c9ad236b7ec6fcd4e
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-09-01 20:47:48 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -1,7 +1,18 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
-from youtubesearchpython import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from config import BANNED_USERS
@@ -12,21 +23,25 @@ from YukkiMusic.misc import SUDOERS
 from YukkiMusic.plugins.play.playlist import del_plist_msg
 from YukkiMusic.plugins.sudo.sudoers import sudoers_list
 from YukkiMusic.utils.database import (add_served_chat,
-                                       add_served_user, get_assistant,
-                                       get_lang, get_userss,
-                                       is_on_off,
+                                       add_served_user,
+                                       blacklisted_chats,
+                                       get_assistant, get_lang,
+                                       get_userss, is_on_off,
                                        is_served_private_chat)
-from YukkiMusic.utils.decorators.language import language
+from YukkiMusic.utils.decorators.language import LanguageStart
 from YukkiMusic.utils.inline import (help_pannel, private_panel,
                                      start_pannel)
+
+loop = asyncio.get_running_loop()
 
 
 @app.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.private
+    & ~filters.edited
     & ~BANNED_USERS
 )
-@language
+@LanguageStart
 async def start_comm(client, message: Message, _):
     await add_served_user(message.from_user.id)
     if len(message.text.split()) > 1:
@@ -40,47 +55,57 @@ async def start_comm(client, message: Message, _):
             return await message.reply_text(_["song_2"])
         if name[0:3] == "sta":
             m = await message.reply_text(
-                "🔎 Fetching your personal stats !"
+                "🔎 Fetching your personal stats.!"
             )
             stats = await get_userss(message.from_user.id)
             tot = len(stats)
-            if tot > 10:
-                tracks = 10
-            else:
-                tracks = tot
             if not stats:
+                await asyncio.sleep(1)
                 return await m.edit(_["ustats_1"])
-            msg = ""
-            limit = 0
-            results = {}
-            for i in stats:
-                top_list = stats[i]["spot"]
-                results[str(i)] = top_list
-                list_arranged = dict(
-                    sorted(
-                        results.items(),
-                        key=lambda item: item[1],
-                        reverse=True,
+
+            def get_stats():
+                msg = ""
+                limit = 0
+                results = {}
+                for i in stats:
+                    top_list = stats[i]["spot"]
+                    results[str(i)] = top_list
+                    list_arranged = dict(
+                        sorted(
+                            results.items(),
+                            key=lambda item: item[1],
+                            reverse=True,
+                        )
                     )
+                if not results:
+                    return m.edit(_["ustats_1"])
+                tota = 0
+                videoid = None
+                for vidid, count in list_arranged.items():
+                    tota += count
+                    if limit == 10:
+                        continue
+                    if limit == 0:
+                        videoid = vidid
+                    limit += 1
+                    details = stats.get(vidid)
+                    title = (details["title"][:35]).title()
+                    if vidid == "telegram":
+                        msg += f"🔗[Telegram Files and Audios](https://t.me/telegram) ** played {count} times**\n\n"
+                    else:
+                        msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
+                msg = _["ustats_2"].format(tot, tota, limit) + msg
+                return videoid, msg
+
+            try:
+                videoid, msg = await loop.run_in_executor(
+                    None, get_stats
                 )
-            if not results:
-                return await m.edit(_["ustats_1"])
-            tota = 0
-            for vidid, count in list_arranged.items():
-                tota += count
-                if limit > 9:
-                    continue
-                if limit == 0:
-                    thumbnail = await YouTube.thumbnail(vidid, True)
-                limit += 1
-                details = stats.get(vidid)
-                title = (details["title"][:35]).title()
-                if vidid == "telegram":
-                    msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ꜰɪʟᴇs & ᴀᴜᴅɪᴏs​](https://t.me/telegram) ** ᴘʟᴀʏᴇᴅ​ {count} ᴛɪᴍᴇs​**\n\n"
-                else:
-                    msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** ᴘʟᴀʏᴇᴅ​ {count} ᴛɪᴍᴇs​**\n\n"
+            except Exception as e:
+                print(e)
+                return
+            thumbnail = await YouTube.thumbnail(videoid, True)
             await m.delete()
-            msg = _["ustats_2"].format(tot, tota, tracks) + msg
             await message.reply_photo(photo=thumbnail, caption=msg)
             return
         if name[0:3] == "sud":
@@ -90,7 +115,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} ʜᴀs ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ​ <code>sᴜᴅᴏʟɪsᴛ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀ ɴᴀᴍᴇ:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>SUDOLIST</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
             return
         if name[0:3] == "lyr":
@@ -106,11 +131,11 @@ async def start_comm(client, message: Message, _):
         if name[0:3] == "del":
             await del_plist_msg(client=client, message=message, _=_)
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("🔎 Fetching Info!")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
-            for result in results.result()["result"]:
+            for result in (await results.next())["result"]:
                 title = result["title"]
                 duration = result["duration"]
                 views = result["viewCount"]["short"]
@@ -122,26 +147,26 @@ async def start_comm(client, message: Message, _):
                 link = result["link"]
                 published = result["publishedTime"]
             searched_text = f"""
-🔍__**ᴛʀᴀᴄᴋ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ**__
+🔍__**Video Track Information**__
 
-🎶**ᴛɪᴛʟᴇ:** {title}
+❇️**Title:** {title}
 
-⏳**ᴅᴜʀᴀᴛɪᴏɴ:** {duration} ᴍɪɴᴜᴛᴇs
-👀**ᴠɪᴇᴡs:** `{views}`
-⏰**ᴘᴜʙʟɪsʜᴇᴅ ᴛɪᴍᴇ:** {published}
-🎥**ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ:** {channel}
-📎**ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ:** [ᴄʟɪᴄᴋ ᴛᴏ ᴠɪsɪᴛ]({channellink})
-🔗**ᴠɪᴅᴇᴏ ʟɪɴᴋ​:** [ᴛᴀᴋᴇ ᴀ ʟᴏᴏᴋ ᴀᴛ ʏᴏᴜᴛᴜʙᴇ​]({link})
+⏳**Duration:** {duration} Mins
+👀**Views:** `{views}`
+⏰**Published Time:** {published}
+🎥**Channel Name:** {channel}
+📎**Channel Link:** [Visit From Here]({channellink})
+🔗**Video Link:** [Link]({link})
 
-⚡️ **sᴇᴀʀᴄʜ ᴘᴏᴡᴇʀᴇᴅ ʙʏ {config.MUSIC_BOT_NAME} **"""
+⚡️ __Searched Powered By {config.MUSIC_BOT_NAME}__"""
             key = InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(
-                            text="• ʏᴏᴜᴛᴜʙᴇ •", url=f"{link}"
+                            text="🎥 Watch ", url=f"{link}"
                         ),
                         InlineKeyboardButton(
-                            text="• ᴄʟᴏsᴇ​ •", callback_data="close"
+                            text="🔄 Close", callback_data="close"
                         ),
                     ],
                 ]
@@ -159,7 +184,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} ʜᴀs ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ​ <code>ᴛʀᴀᴄᴋ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀ ɴᴀᴍᴇ:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>VIDEO INFORMATION</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
     else:
         try:
@@ -192,16 +217,17 @@ async def start_comm(client, message: Message, _):
             sender_name = message.from_user.first_name
             return await app.send_message(
                 config.LOG_GROUP_ID,
-                f"{message.from_user.mention} ʜᴀs ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ʙᴏᴛ.\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀ ɴᴀᴍᴇ:** {sender_name}",
+                f"{message.from_user.mention} has just started Bot.\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
             )
 
 
 @app.on_message(
     filters.command(get_command("START_COMMAND"))
     & filters.group
+    & ~filters.edited
     & ~BANNED_USERS
 )
-@language
+@LanguageStart
 async def testbot(client, message: Message, _):
     out = start_pannel(_)
     return await message.reply_text(
@@ -221,7 +247,7 @@ async def welcome(client, message: Message):
     if config.PRIVATE_BOT_MODE == str(True):
         if not await is_served_private_chat(message.chat.id):
             await message.reply_text(
-                "**ᴛʜɪs ʙᴏᴛ ɪs ᴘʀɪᴠᴀᴛᴇ​**\n\nᴛʜɪs ʙᴏᴛ ᴄᴀɴ ᴏɴʟʏ ʙᴇ ᴜsᴇᴅ ɪɴ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴄʜᴀᴛs​. ɢᴏ ᴛᴏ ᴏᴡɴᴇʀ ᴅᴍ ᴀɴᴅ ᴅɪsᴛᴜʀʙ ʜɪᴍ ꜰᴏʀ ᴀʟʟᴏᴡɪɴɢ ʏᴏᴜʀ ᴄʜᴀᴛ​. 🥱"
+                "**Private Music Bot**\n\nOnly for authorized chats from the owner. Ask my owner to allow your chat first."
             )
             return await app.leave_chat(message.chat.id)
     else:
@@ -235,6 +261,13 @@ async def welcome(client, message: Message):
                 if chat_type != "supergroup":
                     await message.reply_text(_["start_6"])
                     return await app.leave_chat(message.chat.id)
+                if chat_id in await blacklisted_chats():
+                    await message.reply_text(
+                        _["start_7"].format(
+                            f"https://t.me/{app.username}?start=sudolist"
+                        )
+                    )
+                    return await app.leave_chat(chat_id)
                 userbot = await get_assistant(message.chat.id)
                 out = start_pannel(_)
                 await message.reply_text(
